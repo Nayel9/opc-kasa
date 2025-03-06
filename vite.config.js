ccf422c98359d099be70c0d80f8e619907b6ce22
Nahuel Vainer <nahuel9@hotmail.fr>
2025-03-06 09:50:53 +0100

    rassemblement styles + composants

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Kasa-opc/',
+  base: '/opc-kasa/',
   build: {
     sourcemap: false,
   },
